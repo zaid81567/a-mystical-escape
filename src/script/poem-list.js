3fd3ade98a8aa6a74390bd03e37be72cdc60e0e8
zaid <zaid81567@gamil.com>
2024-03-09 00:09:48 +0530

    added poem-list-page and all functionality

---
+++ src/script/poem-list.js
@@ -0,0 +1,72 @@
+//after redirect getting name of the collection using URLSearchParams.get()
+let collection_name = new URLSearchParams(window.location.search).get("value");
+
+const collection_title = document.getElementById("collection-title");
+const poem_list_el = document.getElementById("poem-list");
+
+//setting collection name
+collection_title.innerText = collection_name.toUpperCase().replace("-", " ");
+
+//POEM_LIST====================================================
+async function fetchPoemList(collection) {
+  console.log("inside ShowList:", collection);
+  try {
+    const response = await fetch("../../assets/json/poemList.json");
+    // const response = await fetch("example.json");
+    if (!response.ok) {
+      throw new Error("Response was not ok!");
+    }
+    data = await response.json();
+    showPoemList(data.chapters);
+  } catch (error) {
+    console.log("something went wrong while fetching: ", error);
+  }
+}
+
+function showPoemList(data) {
+  let poem_list = null;
+
+  //get poem_list from json
+  for (let i = 0; i < data.length; i++) {
+    // console.log(data[i]);
+    if (data[i].title == collection_name) {
+      poem_list = data[i].poems;
+      break;
+    }
+  }
+
+  //iterate and append to poem-list
+  for (let i = 0; i < poem_list.length; i++) {
+    // console.log(poem_list[i].title);
+    let poem_title = poem_list[i].title;
+    let poem_title_div = getPoemListEl(poem_title);
+    poem_list_el.appendChild(poem_title_div);
+  }
+}
+
+function getPoemListEl(title) {
+  const poem_list_div = document.createElement("div");
+  poem_list_div.classList.add("poem-list-title", "padding");
+  poem_list_div.setAttribute("data-title", title);
+  const poem_list_div_p = document.createElement("p");
+  poem_list_div_p.innerText = title;
+  poem_list_div_p.setAttribute("data-title", title);
+  poem_list_div.appendChild(poem_list_div_p);
+
+  poem_list_div.addEventListener("click", function (event) {
+    // console.log("clicked", event.target.getAttribute("data-catagory"));
+    let title = event.target.getAttribute("data-title");
+      if (title != null) {
+        setTimeout(() => {
+          //sending collection name with url string
+          window.location.href =
+            "../src/pages/poem-list.html?collection=" + collection+"&poem-title"+title;
+        }, 200);
+      }
+  });
+
+  return poem_list_div;
+}
+
+//fucntions calls
+fetchPoemList(collection_name);
--- src/script/script.js
+++ src/script/script.js
@@ -1,15 +1,22 @@
+//====variables============================================
+
 const collections = document.getElementById("collections");
+const poem_list_container = document.getElementById("poem-list-container");
 
-collections.addEventListener("click", function (event) {
-  console.log("clicked", event.target.getAttribute("data-catagory"));
-  let collection = event.target.getAttribute("data-catagory");
-  //redirects
-  if (collection != null) {
-    setTimeout(() => {
-      window.location.href = "../src/pages/poems-list.html/";
-    }, 200);
-    // window.replace("../src/pages/poems-list.html/");
-  }
-});
+let data = null;
 
-function showPoemList() {}
+//====methods==============================================
+//redirects to poem list
+if (collections) {
+  collections.addEventListener("click", function (event) {
+    // console.log("clicked", event.target.getAttribute("data-catagory"));
+    let collection = event.target.getAttribute("data-catagory");
+    if (collection != null) {
+      setTimeout(() => {
+        //sending collection name with url string
+        window.location.href =
+          "../src/pages/poem-list.html?value=" + collection;
+      }, 200);
+    }
+  });
+}
